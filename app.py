1b6f2210348b236f1f202f0372f23e8bd9d95c5e
ggezy <61896063+ggezy@users.noreply.github.com>
2024-06-05 11:35:12 +0800

    Update app.py
    editing login function and adding logout function

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from Crypto.Random import get_random_bytes
+from user import User
 import encryption
 import base64
 
 app = Flask(__name__)
-"""
-Process Flow: Login -> Send Message (Send into messages pool contains (User, Encrypted Message, HMAC) -> Receiver Message via pooling (Decrypt the inputted Message)
-"""
+
 # Secret key for encryption and HMAC (16 bytes for AES-128)
 SECRET_KEY = get_random_bytes(32)
-#login_manager = LoginManager()
-#login_manager.init_app(app)
-#login_manager.login_view = 'login'
 app.config['SECRET_KEY'] = SECRET_KEY
 
-messages = []
+# Setup Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
+messages = []
 users = {
     "admin": "admin",
     "minda": "minda1",
@@ -24,40 +24,47 @@ users = {
     "naya": "naya1"
 }
 
+@login_manager.user_loader
+def load_user(username):
+    if username in users:
+        return User(username, users[username])
+    return None
+
 # Route for serving HTML templates and handling form submissions
 @app.route('/')
 def index():
     return render_template('index.html')
-
-#@login_manager.user_loader
-#def load_user(user_id):
-#    for user in users.values():
-#        if user.id == int(user_id):
-#            return user
-#    return None
-
-#Hash matching
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if users.get(username, None) is not None and users.get(username, None) == password:
-            login_user(username)
-            return redirect(url_for('index'))
+        # if users.get(username, None) is not None and users.get(username, None) == password:
+        #     login_user(username)
+        #     return redirect(url_for('chat'))
+        if username in users and users[username] == password:
+            user = User(username, password)
+            login_user(user)
+            return redirect(url_for('chat'))
         else:
             flash('Invalid username or password')
     return render_template('login.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/chat')
+@login_required
+def chat():
+    return render_template('chat.html')
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    """
-    Send message, pool into global messages
-    :return:
-    """
     user = request.form.get('user')
     message = request.form.get('message')
 
@@ -74,10 +81,6 @@ def send_message():
 
 @app.route('/receive_messages', methods=['GET'])
 def receive_messages():
-    """
-    retrieve message from global messages, re-iterate over and over (pooling)
-    :return:
-    """
     decrypted_messages = []
     for msg in messages:
         if encryption.EncryptionHelper.verify_hmac(ciphertext=base64.b64decode(msg['message']), secretkey=SECRET_KEY, received_hmac=msg['hmac_signature']):
